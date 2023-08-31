4bc059cba351236f9b68173d5b31e9dd1a6011d8
DevAro178 <me.ammad1786@gmail.com>
2023-08-31 13:26:25 +0500

    Reverted Back to Persist and AuthTokens inside the userData Slice

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -18,18 +18,6 @@ import { persistor } from "../redux/store";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  let authTokens = localStorage.getItem("authTokens")
-    ? JSON.parse(localStorage.getItem("authTokens"))
-    : null;
-  // let userId = authTokens?.user_id || null;
-
-  // let user = authTokens?.access || null;
-  // let userEmail = authTokens?.email || null;
-  // let userName = authTokens?.username || null;
-  let isLoggedIn = localStorage.getItem("authTokens") ? true : false;
-
-  // console.log(authTokens, userId, user, userEmail, userName);
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -38,7 +26,8 @@ export const AuthProvider = ({ children }) => {
     userId,
     userName,
     userEmail,
-    // isLoggedIn,
+    authTokens,
+    isLoggedIn,
     loading,
     userData,
     prevLocation,
@@ -59,7 +48,7 @@ export const AuthProvider = ({ children }) => {
     let data = await response.json();
 
     if (data) {
-      localStorage.setItem("authTokens", JSON.stringify(data));
+      dispatch(setAuthTokens(data));
       dispatch(setUser(data.access));
       dispatch(setUserId(data.user_id));
       dispatch(setUserEmail(data.email));
@@ -76,40 +65,28 @@ export const AuthProvider = ({ children }) => {
   };
 
   let logoutUser = () => {
-    // console.log("Logout triggered");
-    localStorage.removeItem("authTokens");
     dispatch(resetUserData());
     persistor.purge();
-    // dispatch(setAuthTokens(null));
-    // dispatch(setUser(null));
-    // dispatch(setUserId(null));
-    // dispatch(setUserEmail(null));
-    // dispatch(setUserName(null));
-    // dispatch(setIsLoggedIn(false));
   };
 
-  const updateToken = async (token) => {
-    if (token) {
-      // console.log("Update Token Running");
+  const updateToken = async () => {
+    const response = await fetch("http://127.0.0.1:8000/token/refresh/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refresh: authTokens?.refresh }),
+    });
 
-      const response = await fetch("http://127.0.0.1:8000/token/refresh/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ refresh: token }),
-      });
+    const data = await response.json();
+    if (response.status === 200) {
+      dispatch(setAuthTokens(data));
+    } else {
+      logoutUser();
+    }
 
-      const data = await response.json();
-      if (response.status === 200) {
-        localStorage.setItem("authTokens", JSON.stringify(data));
-      } else {
-        logoutUser();
-      }
-
-      if (loading) {
-        dispatch(setLoading(false));
-      }
+    if (loading) {
+      dispatch(setLoading(false));
     }
   };
 
@@ -133,13 +110,7 @@ export const AuthProvider = ({ children }) => {
     const REFRESH_INTERVAL = 1000 * 60 * 59; // 24 minutes
     let interval = setInterval(() => {
       if (authTokens) {
-        let UpdateAuthTokens = localStorage.getItem("authTokens")
-          ? JSON.parse(localStorage.getItem("authTokens"))
-          : null;
-        let refreshToken = UpdateAuthTokens?.refresh;
-        // console.log(refreshToken);
-
-        updateToken(refreshToken);
+        updateToken();
       }
     }, REFRESH_INTERVAL);
     return () => clearInterval(interval);
